0d0b91efa46e0de737ee582d4f6bbcb51f30c99c
Daan Boerlage <dboerlage@cimpress.com>
2018-11-26 15:33:16 +0100

    Rework typings file again so it actually works

--- index.d.ts
+++ index.d.ts
@@ -1,59 +1,48 @@
-declare class Response {
-  constructor(body: object, statusCode: number, headers: object): void;
+declare module 'openapi-factory' {
+  class OpenApi {
+    constructor(options: OpenApi.ApiFactoryOptions, overrideLogger?: Function);
+
+    setAuthorizer(authorizerFunc: Function): void;
+
+    onEvent(onEventFunc: Function): void;
+
+    onSchedule(onScheduleFunc: Function): void;
+
+    head(route: string, p0: Function): void;
+    head(route: string, p0: object, p1: Function): void;
+
+    get(route: string, p0: Function): void;
+    get(route: string, p0: object, p1: Function): void;
+
+    post(route: string, p0: Function): void;
+    post(route: string, p0: object, p1: Function): void;
+
+    put(route: string, p0: Function): void;
+    put(route: string, p0: object, p1: Function): void;
+
+    patch(route: string, p0: Function): void;
+    patch(route: string, p0: object, p1: Function): void;
+
+    delete(route: string, p0: Function): void;
+    delete(route: string, p0: object, p1: Function): void;
+
+    options(route: string, p0: Function): void;
+    options(route: string, p0: object, p1: Function): void;
+
+    any(route: string, p0: Function): void;
+    any(route: string, p0: object, p1: Function): void;
+
+    handler(event: object, context: object): Promise<any>;
+  }
+
+  namespace OpenApi {
+    interface ApiFactoryOptions {
+      requestMiddleware?: Function,
+      responseMiddleware?: Function,
+      errorMiddleware?: Function
+    }
+  }
+
+  export = OpenApi;
 }
 
-declare interface ApiFactoryOptions {
-  requestMiddleware: Function,
-  responseMiddleware: Function,
-  errorMiddleware: Function
-}
-
-declare interface ILogger {
-  log(...args: any[]): void;
-  trace(...args: any[]): void;
-  debug(...args: any[]): void;
-  info(...args: any[]): void;
-  warn(...args: any[]): void;
-  error(...args: any[]): void;
-  fatal(...args: any[]): void;
-}
-
-declare function AuthorizerFunc(event: object): boolean {}
-declare function OnEventFunc(event: object, context?: object): any {}
-declare function OnScheduleFunc(event: object, context?: object): any {}
-
-declare class ApiFactory {
-  constructor(options: ApiFactoryOptions, overrideLogger?: ILogger);
-
-  setAuthorizer(authorizerFunc: AuthorizerFunc): void;
-
-  onEvent(onEventFunc: OnEventFunc): void;
-
-  onSchedule(onScheduleFunc: Function): void;
-
-  head(route: string, p0: Function|object): void;
-  head(route: string, p0: object, p1: Function): void;
-
-  get(route: string, p0: Function): void;
-  get(route: string, p0: object, p1: Function): void;
-
-  post(route: string, p0: Function): void;
-  post(route: string, p0: object, p1: Function): void;
-
-  put(route: string, p0: Function): void;
-  put(route: string, p0: object, p1: Function): void;
-
-  patch(route: string, p0: Function): void;
-  patch(route: string, p0: object, p1: Function): void;
-
-  delete(route: string, p0: Function): void;
-  delete(route: string, p0: object, p1: Function): void;
-
-  options(route: string, p0: Function): void;
-  options(route: string, p0: object, p1: Function): void;
-
-  any(route: string, p0: Function): void;
-  any(route: string, p0: object, p1: Function): void;
-
-  async handler(event: object, context: object): Response;
-}
