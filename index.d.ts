7a37f8b8f91d68df9201b3039216538b68975956
Daan Boerlage <dboerlage@cimpress.com>
2018-11-26 10:52:50 +0100

    Rewrite ts declaration file with feedback from library author

--- index.d.ts
+++ index.d.ts
@@ -1,33 +1,59 @@
-declare class MapExpander {
-  expandMap(currentMap: Object, pathString: string, mapValue: Object): Object;
-  getMapValue(currentMap: Object, pathString: string): Object;
+declare class Response {
+  constructor(body: object, statusCode: number, headers: object): void;
 }
 
-declare class Response {
-  constructor(body: Object, statusCode: number, headers: Object): void;
+declare interface ApiFactoryOptions {
+  requestMiddleware: Function,
+  responseMiddleware: Function,
+  errorMiddleware: Function
 }
+
+declare interface ILogger {
+  log(...args: any[]): void;
+  trace(...args: any[]): void;
+  debug(...args: any[]): void;
+  info(...args: any[]): void;
+  warn(...args: any[]): void;
+  error(...args: any[]): void;
+  fatal(...args: any[]): void;
+}
+
+declare function AuthorizerFunc(event: object): boolean {}
+declare function OnEventFunc(event: object, context?: object): any {}
+declare function OnScheduleFunc(event: object, context?: object): any {}
 
 declare class ApiFactory {
-  constructor(options: object, overrideLogger: object);
+  constructor(options: ApiFactoryOptions, overrideLogger?: ILogger);
 
+  setAuthorizer(authorizerFunc: AuthorizerFunc): void;
 
-  setAuthorizer(authorizerFunc: any): void;
-  SetAuthorizer(authorizerFunc: any): void;
+  onEvent(onEventFunc: OnEventFunc): void;
 
-  onEvent(onEventFunc: any): void;
+  onSchedule(onScheduleFunc: Function): void;
 
-  onSchedule(onScheduleFunc: any): void;
+  head(route: string, p0: Function|object): void;
+  head(route: string, p0: object, p1: Function): void;
 
-  head(route: string, p0: Function, p1: Function): void;
-  get(route: string, p0: Function, p1: Function): void;
-  post(route: string, p0: Function, p1: Function): void;
-  put(route: string, p0: Function, p1: Function): void;
-  patch(route: string, p0: Function, p1: Function): void;
-  delete(route: string, p0: Function, p1: Function): void;
-  options(route: string, p0: Function, p1: Function): void;
-  any(route: string, p0: Function, p1: Function): void;
+  get(route: string, p0: Function): void;
+  get(route: string, p0: object, p1: Function): void;
 
-  method(verb: string, route: string, p0: Function, p1: Function): void;
+  post(route: string, p0: Function): void;
+  post(route: string, p0: object, p1: Function): void;
 
-  async handler(event: Object, context: Object): Response;
+  put(route: string, p0: Function): void;
+  put(route: string, p0: object, p1: Function): void;
+
+  patch(route: string, p0: Function): void;
+  patch(route: string, p0: object, p1: Function): void;
+
+  delete(route: string, p0: Function): void;
+  delete(route: string, p0: object, p1: Function): void;
+
+  options(route: string, p0: Function): void;
+  options(route: string, p0: object, p1: Function): void;
+
+  any(route: string, p0: Function): void;
+  any(route: string, p0: object, p1: Function): void;
+
+  async handler(event: object, context: object): Response;
 }
