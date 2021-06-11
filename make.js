6c9bb6cc3737c09380bd01c0086b4416b56c611a
Warren Parad <wparad@gmail.com>
2021-06-11 16:37:45 +0200

    Convert build to github actions.

--- make.js
+++ make.js
@@ -1,48 +1,64 @@
-'use strict';
-
 /**
  * Module dependencies
  */
-var commander = require('commander');
-var fs = require('fs');
+ let commander = require('commander');
+ let fs = require('fs-extra');
 
-var travis = require('travis-build-tools')(process.env.GIT_TAG_PUSHER);
-var version = travis.GetVersion();
-commander.version(version);
+ const githubActionsRunner = require('ci-build-tools')(process.env.GITHUB_TOKEN);
+ function getVersion() {
+   let release_version = '0.0';
+   const pull_request = '';
+   const branch = process.env.GITHUB_REF;
+   const build_number = `${process.env.GITHUB_RUN_NUMBER}`;
 
-/**
- * Build
- */
-commander
-	.command('build')
-	.description('Setup require build files for npm package.')
-	.action(() => {
-		var package_metadata = require('./package.json');
-		package_metadata.version = version;
-		fs.writeFileSync('./package.json', JSON.stringify(package_metadata, null, 2));
+   //Builds of pull requests
+   if (pull_request && !pull_request.match(/false/i)) {
+     release_version = `0.${pull_request}`;
+   } else if (!branch || !branch.match(/^(refs\/heads\/)?release[/-]/i)) {
+     //Builds of branches that aren't master or release
+     release_version = '0.0';
+   } else {
+     //Builds of release branches (or locally or on server)
+     release_version = branch.match(/^(?:refs\/heads\/)?release[/-](\d+(?:\.\d+){0,3})$/i)[1];
+   }
+   return `${release_version}.${(build_number || '0')}.0.0.0.0`.split('.').slice(0, 3).join('.');
+ }
+ const version = getVersion();
+ commander.version(version);
 
-		console.log("Building package %s (%s)", package_metadata.name, version);
-		console.log('');
-	});
+ /**
+  * Build
+  */
+ commander
+ .command('setup')
+ .description('Setup require build files for npm package.')
+ .action(async () => {
+   let package_metadata = require('./package.json');
+   package_metadata.version = version;
+   await fs.writeJson('./package.json', package_metadata, { spaces: 2 });
 
-/**
- * After Build
- */
-commander
-	.command('after_build')
-	.description('Publishes git tags and reports failures.')
-	.action(() => {
-		var package_metadata = require('./package.json');
-		console.log("After build package %s (%s)", package_metadata.name, version);
-		console.log('');
-		travis.PublishGitTag();
-		travis.MergeDownstream('release/', 'master');
-	});
+   console.log('Building package %s (%s)', package_metadata.name, version);
+   console.log('');
+ });
 
-commander.on('*', () => {
-	if(commander.args.join(' ') == 'tests/**/*.js') { return; }
-	console.log('Unknown Command: ' + commander.args.join(' '));
-	commander.help();
-	process.exit(0);
-});
-commander.parse(process.argv[2] ? process.argv : process.argv.concat(['build']));
+ /**
+  * After Build
+  */
+ commander
+ .command('after_build')
+ .description('Publishes git tags and reports failures.')
+ .action(() => {
+   let package_metadata = require('./package.json');
+   console.log('After build package %s (%s)', package_metadata.name, version);
+   console.log('');
+   githubActionsRunner.PublishGitTag(version);
+   githubActionsRunner.MergeDownstream('release/', 'main');
+ });
+
+ commander.on('*', () => {
+   if (commander.args.join(' ') === 'tests/**/*.js') { return; }
+   console.log(`Unknown Command: ${commander.args.join(' ')}`);
+   commander.help();
+   process.exit(0);
+ });
+ commander.parse(process.argv[2] ? process.argv : process.argv.concat(['build']));
